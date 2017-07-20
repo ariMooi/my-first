1f7a4e8f40567871793d420b6dc498a74620ab74
Luis Zepeda <lzmonter@gmail.com>
2017-07-20 03:32:09 -0500

    se agrega reconocimiento de voz

--- main.js
+++ main.js
@@ -48,7 +48,6 @@ function deleteFruit(fruta){
   frutas.map(f => { listFruit(f) })
 }
 
-
 function addFruit(){
   var newF = document.getElementById("newFruit").value
   frutas.push(newF)
@@ -61,3 +60,55 @@ function addFruit(){
 }
 
 frutas.map(f => { listFruit(f) })
+
+
+
+//reconocimiento de voz
+
+var recognition;
+var recognizing = false;
+
+//verificacion de existencia del objeto webkitSpeechRecognition
+if (!('webkitSpeechRecognition' in window)) {
+  alert("¡API no soportada!");
+} else {
+
+  recognition = new webkitSpeechRecognition();
+  recognition.lang = "es-VE";
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  //Este evento se ejecuta cuando la función start() es invocada el navegador empieza a escuchar
+  recognition.onstart = function() {
+    recognizing = true;
+    console.log("empezando a escuchar");
+  }
+
+  //este evento devuelve el resultado obtenido en texto plano.
+  recognition.onresult = function(event) {
+   for (var i = event.resultIndex; i < event.results.length; i++) {
+    if(event.results[i].isFinal)
+      document.getElementById("newFruit").value += event.results[i][0].transcript;
+      }
+    //texto
+  }
+
+  recognition.onerror = function(event) {
+  }
+
+  //Este evento se ejecuta cuando el usuario ha acabado de hablar
+  recognition.onend = function() {
+    recognizing = false;
+    console.log("terminó de escuchar, llegó a su fin");
+  }
+}
+
+function procesar() {
+  if (recognizing == false) {
+    recognition.start();
+    recognizing = true;
+  } else {
+    recognition.stop();
+    recognizing = false;
+  }
+}
